79c949a2e59fd670a82cb58f8f9d57f0023e35e9
Change Agent <keasiley@icloud.com>
2023-06-04 12:17:28 -0500

    node packages added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
+const WorkboxPlugin = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
@@ -17,10 +17,14 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
+    plugins: [WorkboxPlugin.GenerateSW
       
     ],
-
+    new WorkboxPlugin({
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+  
     module: {
       rules: [
         
