e1405bdc2b256771f19ddddef727a81da818f846
Change Agent <keasiley@icloud.com>
2023-06-06 06:23:01 -0500

    database.js and install.js edited om the src folder

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,28 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+    butInstall.classList.toggle('hidden');
+});
+
+    // TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    promptEvent.prompt();
+    promptEvent.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the Add to Home Screen prompts');
+        } else {
+            console.log('User dismissed the Add to Home Screen prompts');
+        }
+        window.deferredPrompt = null;
+        butInstall.classList.toggle('hidden');
+    });
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
