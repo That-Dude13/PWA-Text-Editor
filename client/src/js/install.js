89b5bed43deaf21fcdc9e5301dd45dfb558f3745
Change Agent <keasiley@icloud.com>
2023-06-08 13:11:38 -0500

    ToDos removed and commented out code

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,30 +1,25 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-    window.deferredPrompt = event;
+window.addEventListener("beforeinstallprompt", (event) => {
+  window.deferredPrompt = event;
 
-    butInstall.classList.toggle('hidden');
+  butInstall.classList.toggle("hidden");
 });
 
-    // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
-    promptEvent.prompt();
-    promptEvent.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the Add to Home Screen prompts');
-        } else {
-            console.log('User dismissed the Add to Home Screen prompts');
-        }
-        window.deferredPrompt = null;
-        butInstall.classList.toggle('hidden');
-    });
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+  promptEvent.prompt();
+  promptEvent.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === "accepted") {
+      console.log("User accepted the Add to Home Screen prompts");
+    } else {
+      console.log("User dismissed the Add to Home Screen prompts");
+    }
+    window.deferredPrompt = null;
+    butInstall.classList.toggle("hidden");
+  });
 });
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {
-    console.log('Appinstalled');
-
+window.addEventListener("appinstalled", (event) => {
+  console.log("Appinstalled");
 });
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,89 +1,73 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-const { GenerateSW } = require('workbox-webpack-plugin');
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const { GenerateSW } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'production',
+    mode: "production",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true
+        clientsClaim: true,
+        skipWaiting: true,
       }),
-      ],
-      plugins: [
-        new HtmlWebpackPlugin({
-          template: './index.html',
-          filename: './index.html'
-        }),
-      
-        
-        new InjectManifest({
-          swSrc: './src-sw.js',
-          swDest:'src-sw.js',
-          // globDirectory: 'dist/',
-          // globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,gif,svg,woff,woff2,ttf,eot}'],
-          // maximumFileSizeToCacheInBytes: 50000000,
-          // maximumFileSizeToCacheOnDiskInBytes: 50000000,
-        }),
-      
-      // const generateManifest = ({ name, short_name, description, background_color, theme_color, start_url, publicPath, icons }) => {
-        
-          new WebpackPwaManifest({
-        
-            name: 'PwaTexEditor',
-            short_name: 'Edit',
-            description: 'Automatically edit your text',
-            background_color: '#225ca3',
-            theme_color: '#225ca3',
-            start_url: './',
-            publicPath: './',
-            icons: [
-              {
-                src: path.resolve('src/images/logo.png'),
-                sizes: [96, 128, 192, 256, 384, 512],
-                destination: path.join('assets', 'icons'),
-              },
-            ],
-          }),
-      ],
-      
-      
-  
+    ],
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        filename: "./index.html",
+      }),
+
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+
+      new WebpackPwaManifest({
+        name: "PwaTexEditor",
+        short_name: "Edit",
+        description: "Automatically edit your text",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+    ],
+
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
 
         {
           test: /\.(?:js|mjs|cjs)$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: [
-                ['@babel/preset-env', { targets: "defaults" }]
-              
-        
-              ]
-            }
-          }
-        }
-      ]
-    }
-  }
+              presets: [["@babel/preset-env", { targets: "defaults" }]],
+            },
+          },
+        },
+      ],
+    },
+  };
 };
